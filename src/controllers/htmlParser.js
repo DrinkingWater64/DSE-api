f092efa8076eee91a0da2030363e9c1aeb42aa92
akshar <61174663+DrinkingWater64@users.noreply.github.com>
2024-10-29 15:29:59 +0600

    implement fetch, removed depricated request

--- src/controllers/htmlParser.js
+++ src/controllers/htmlParser.js
@@ -42,100 +42,110 @@ async function getCompanyList(callback) {
     }
 }
 
-// const getLatestStockPrice = (callback) => {
-//     request.get('https://www.dsebd.org/latest_share_price_scroll_l.php', (error, res, body) => {
-//         if (error) {
-//             callback({ error: error })
-//         }
-//         else {
-//             var parsedStocks = {
-//                 date: Date.now(),
-//                 stocks: []
-//             }
-//             const root = htmlParser.parse(body);
-//             var stocks = root.querySelectorAll('.ab1')
+async function getLatestStockPrice(callback) {
+    try{
+        const response = await fetch('https://www.dsebd.org/latest_share_price_scroll_l.php');
+        if (!response.ok){
+            throw new Error("Network response was not ok" + response.statusText);
+        }
+
+        const body = await response.text();
+        
+        let parsedStocks = {
+            data: Date.now(),
+            stocks: []
+        }
+
+        const root = htmlParser.parse(body);
+        const stocks = root.querySelectorAll('.ab1');
+
+        for (let i = 0; i < 360; i++) {
+            parsedStocks.stocks.push(getStockDetails(stocks[i]));
+        }
+
+        callback(parsedStocks);
+
+    }
+    catch(error){
+        console.error('Error occured', error);
+    }
+    
+}
 
 
-//             for (var i = 0; i < 360; i++) {
-//                 parsedStocks.stocks.push(getStockDetails(stocks[i]))
-//             }
-//             callback(parsedStocks)
-//         }
-//     })
-// }
+async function company_price_data(name, type, durationInput, callback) {
+    try{
+        let valid_durations = [1, 3, 6, 9, 12, 24];
+        let valid_types = ['price', 'vol', 'trd'];
 
-// const company_price_data = (name, type, duration, callback) => {
-//     var valid_durations = [1, 3, 6, 9, 12, 24];
-//     var valid_types = ['price', 'vol', 'trd'];
-
-//     // validity_check required
-
-//     var duration = duration.toString()
-//     request.get("https://www.dsebd.org/php_graph/monthly_graph.php?inst=" + name + "&duration=" + duration + "&type=" + type, (error, res, body) => {
-//         if (error) {
-//             callback({ error: error })
-//         }
-//         else {
-//             var findStr = "// CSV or path to a CSV file.";
-//             var endStr = "// options go here."
-//             var start_index = body.indexOf(findStr) + findStr.length
-//             var end_index = body.indexOf(endStr)
-
-//             csv_file = body.substr(start_index, end_index - start_index).trim().replace('{', "")
-//             price_list = csv_file.split('+')
-//             final_list = []
-//             for (var i = 1; i < price_list.length; i++) {
-//                 var values = price_list[i].replace("\"", "").trim().split('\\')[0].split(',')
-//                 final_list.push({ date: values[0], data: values[1] })
-//             }
-//             callback(final_list)
-//         }
-//     });
-// }
+        const duration = durationInput.toString(); // Use a different variable name
+        const response = await fetch(`https://www.dsebd.org/php_graph/monthly_graph.php?inst=${name}&duration=${duration}&type=${type}`);
 
 
-// async function company_price_data(name, type, duration, callback) {
-//     try{
-//         let valid_durations = [1, 3, 6, 9, 12, 24];
-//         let valid_types = ['price', 'vol', 'trd'];
-//     }
-//     catch(error){
+        if (!response.ok){
+            throw new Error("Network response was not ok" + response.statusText);
+        }
 
-//     }
-// }
+        const body = await response.text();
+        const findStr = "// CSV or path to a CSV file.";
+        const endStr = "// options go here."
+        const start_index = body.indexOf(findStr) + findStr.length
+        const end_index = body.indexOf(endStr)
 
-// const company_details = (name, callback) => {
-//     request.get('https://www.dsebd.org/displayCompany.php?name=' + name, (error, res, body) => {
-//         if (error) {
-//             callback({ error: error })
-//         }
-//         else {
-//             const dom = new JSDOM(body);
-//             var tables = dom.window.document.querySelectorAll('#company')
-//             var market_info = new Object()
-//             var basic_info = new Object()
+        if (start_index < 0 || end_index < 0 || start_index >= end_index) {
+            return callback({ error: 'Failed to extract CSV data from response.' });
+        }
 
-//             // Working on tables[1] Market Information
-//             var market_info_fields = ['LTP', 'CLOSEP', 'LASTUPDATE', 'DR', 'CA', 'DR', 'CP', 'YEARLYMR', 'OP', 'DV', 'AOP', 'DT', 'YCP', 'MC']
+        const csv_file = body.substr(start_index, end_index - start_index).trim().replace('{', "");
+        const price_list = csv_file.split('+');
+        const final_list = []
 
-//             var infos = tables[1].querySelectorAll('td')
+        for(var i = 1; i < price_list.length; i++){
+            var values = price_list[i].replace("\"", "").trim().split('\\')[0].split(',');
+            final_list.push({ date: values[0], data: values[1] });
+        }
 
-//             market_info['LTP'] = parseFloat(infos[0].textContent);
-//             for (var i = 0; i < infos.length; i++) {
-//                 market_info[market_info_fields[i]] = infos[i].textContent.trim()
-//             }
+        callback(final_list);
+    }
+    catch(error){
+        console.error("Error occurred: ", error);
+    }
+}
 
-//             var basic_info_fields = ['AC', 'DTD', 'PC', 'TI', 'FACEPV', 'ML', 'TOS', 'Sector']
 
-//             var basic = tables[2].querySelectorAll('td')
-//             for (var i = 0; i < basic_info_fields.length; i++) {
-//                 basic_info[basic_info_fields[i]] = basic[i].textContent.trim()
-//             }
+async function company_details(name,callback) {
+    try{
+        const response = await fetch('https://www.dsebd.org/displayCompany.php?name=' + name);
+        if (!response.ok){
+            throw new Error("Network response was not ok" + response.statusText);
+        }
+        
+        const body = await response.text();
+            const dom = new JSDOM(body);
+            const tables = dom.window.document.querySelectorAll('#company')
+            let market_info = new Object()
+            const basic_info = new Object()
 
-//             callback({ market_info, basic_info })
-//         }
-//     })
-// }
+            const market_info_fields = ['LTP', 'CLOSEP', 'LASTUPDATE', 'DR', 'CA', 'DR', 'CP', 'YEARLYMR', 'OP', 'DV', 'AOP', 'DT', 'YCP', 'MC']
+            const infos = tables[1].querySelectorAll('td');
+            
+            market_info['LTP'] = parseFloat(infos[0].textContent);
+            for (var i = 0; i < infos.length; i++) {
+                market_info[market_info_fields[i]] = infos[i].textContent.trim();
+            }
 
-// module.exports = { getLatestStockPrice, company_price_data, company_details, getCompanyList } 
-module.exports = { getCompanyList } 
+            let basic_info_fields = ['AC', 'DTD', 'PC', 'TI', 'FACEPV', 'ML', 'TOS', 'Sector'];
+
+            let basic = tables[2].querySelectorAll('td')
+            for (var i = 0; i < basic_info_fields.length; i++) {
+                basic_info[basic_info_fields[i]] = basic[i].textContent.trim()
+            }
+            callback({market_info, basic_info})
+    }
+    catch(error){
+        console.log('Error occured,', error);
+    }
+    
+}
+
+module.exports = { getCompanyList, company_price_data, getLatestStockPrice, company_details } 
--- src/routers/api.js
+++ src/routers/api.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
-// const { getLatestStockPrice, company_price_data, company_details, getCompanyList } = require('../controllers/htmlParser')
-const { getCompanyList } = require('../controllers/htmlParser')
+const { getCompanyList, company_price_data, getLatestStockPrice, company_details} = require('../controllers/htmlParser')
 
 
 
@@ -11,49 +10,49 @@ router.get('/company_list', (req, res) => {
     })
 })
 
-// router.get('/share_price', (req, res) => {
-//     company_name = req.query.name
-//     getLatestStockPrice((out) => {
-//         var stocks = out.stocks
-//         stocks = stocks.filter((e) => e.TRADING_CODE == company_name)
+router.get('/share_price', (req, res) => {
+    company_name = req.query.name
+    getLatestStockPrice((out) => {
+        var stocks = out.stocks
+        stocks = stocks.filter((e) => e.TRADING_CODE == company_name)
 
-//         res.status(200).send(stocks[0])
-//     })
-// })
+        res.status(200).send(stocks[0])
+    })
+})
 
-// router.get('/latest_price', (req, res) => {
-//     getLatestStockPrice((out) => {
-//         if (out.error) {
-//             return res.status(500).send(out.error)
-//         }
-//         else {
-//             return res.status(200).send(out)
-//         }
-//     })
-// })
+router.get('/latest_price', (req, res) => {
+    getLatestStockPrice((out) => {
+        if (out.error) {
+            return res.status(500).send(out.error)
+        }
+        else {
+            return res.status(200).send(out)
+        }
+    })
+})
 
 
-// router.get('/company_details', (req, res) => {
-//     const name = req.query.name
+router.get('/company_details', (req, res) => {
+    const name = req.query.name
 
-//     company_details(name, (value) => {
-//         res.status(200).send(value)
-//     })
-// })
+    company_details(name, (value) => {
+        res.status(200).send(value)
+    })
+})
 
-// router.get('/company_data', (req, res) => {
-//     const name = req.query.name
-//     const type = req.query.type
-//     const duration = parseInt(req.query.duration)
-//     console.log(name, type, duration)
-//     company_price_data(name, type, duration, (out) => {
-//         if (out.error) {
-//             return res.status(500).send(out.error)
-//         }
-//         else {
-//             return res.status(200).send(out)
-//         }
-//     })
-// })
+router.get('/company_data', (req, res) => {
+    const name = req.query.name
+    const type = req.query.type
+    const duration = parseInt(req.query.duration)
+    console.log(name, type, duration)
+    company_price_data(name, type, duration, (out) => {
+        if (out.error) {
+            return res.status(500).send(out.error)
+        }
+        else {
+            return res.status(200).send(out)
+        }
+    })
+})
 
 module.exports = router
